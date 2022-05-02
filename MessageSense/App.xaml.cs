f2d88703910d82941cdadd09ba693c28aef6eef5
bluke90 <bganzerla@gmail.com>
2022-05-02 12:57:20 -0500

    Merge Development branch to master

--- MessageSense/App.xaml.cs
+++ MessageSense/App.xaml.cs
@@ -1,27 +1,44 @@
-﻿using Microsoft.Maui;
-
-
-namespace MessageSense;
+﻿namespace MessageSense;
 
 public partial class App : Application
 {
 
-	private AppManager _appManager = null;
+	private AppManager _appManager;
 
 	public App()
 	{
+		_appManager = new AppManager();
+
 		InitializeComponent();
 
-		var user = Preferences.Get("username", null);
-		var fName = Preferences.Get("firstName", null);
-		var lName = Preferences.Get("lastName", null);
-		var appUser = Preferences.Get("appUser", null);
+		// AddTestMessage();
 
-		if (user == null || fName == null || lName == null || appUser == null) {
+		var user = Microsoft.Maui.Essentials.Preferences.Get("username", null);
+		var fName = Microsoft.Maui.Essentials.Preferences.Get("firstName", null);
+		var lName = Microsoft.Maui.Essentials.Preferences.Get("lastName", null);
+
+		if (user == null || fName == null || lName == null)
+		{
 			MainPage = new SetupPage(_appManager);
-		} else {
-			_appManager = new AppManager();
+		} else
+        {
 			MainPage = new MainPage(_appManager);
 		}
 	}
+
+	private void AddTestMessage()
+    {
+		Models.Message message = new Models.Message()
+		{
+			DateTime = DateTime.Now,
+			RecipientToken = "0001",
+			Data = "It's going good! How about yourself??",
+			SenderToken = "18580206",
+			Read = false
+		};
+
+		_appManager.MessageSenseData.Messages.Add(message);
+		_appManager.MessageSenseData.SaveChanges();
+	}
+
 }
